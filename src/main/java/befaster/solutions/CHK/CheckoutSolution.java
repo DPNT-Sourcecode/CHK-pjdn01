78be1d5a0cddb25cf123b647668a486fc6621ac7
sbx_user1051 <sbx_user1051@169.254.33.101>
2024-04-21 14:06:23 +0000

    Sun Apr 21 14:06:23 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,6 @@ package befaster.solutions.CHK;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,35 +13,27 @@ public class CheckoutSolution {
 
     private static final String filename = "D:\\akahuc\\Documents\\runner-for-java-windows\\accelerate_runner\\src\\main\\resources\\itemCatalogue.json";
 
-    private static final Map<ItemType, ItemPrice> catalogue = new HashMap<>();
-
-    public CheckoutSolution() {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            Map<Object, Object> data = mapper.readValue(new FileReader(filename), Map.class);
-            catalogue.putAll(buildCatalogue(data));
-        } catch (Exception e) {
-            System.out.println("Error occurred loading catalogue data");
-            System.exit(-1);
-        }
-
-    }
-
     public static void main(String[] args) {
         Integer check = checkout("FF");
         System.out.println(check);
     }
 
     public static Integer checkout(String skus) {
-        if (skus == null || skus.isEmpty()) {
-            return 0;
+        try {
+            if (skus == null || skus.isEmpty()) {
+                return 0;
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            Map<Object, Object> data = mapper.readValue(new FileReader(filename), Map.class);
+            Map<ItemType, ItemPrice> catalogue = buildCatalogue(data);
+            Map<ItemType, Integer> itemToCountMap = getItemToCountMap(skus);
+            Integer totalCost = calculateTotalCost(itemToCountMap, catalogue);
+            return totalCost;
+        } catch (Exception e) {
+            System.out.println("Error occurred loading catalogue data");
+            return -1;
         }
-
-        Map<ItemType, Integer> itemToCountMap = getItemToCountMap(skus);
-
-        Integer totalCost = calculateTotalCost(itemToCountMap, catalogue);
-        return totalCost;
-
     }
 
     private static Map<ItemType, Integer> getItemToCountMap(String skus) {
@@ -53,4 +44,5 @@ public class CheckoutSolution {
         return itemToCountMap;
     }
 }
+
 
