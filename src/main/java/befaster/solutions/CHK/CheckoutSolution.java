85e721bbaabd15c3322504e188187eddaebb94ee
sbx_user1051 <sbx_user1051@169.254.33.101>
2024-04-21 03:32:10 +0000

    Sun Apr 21 03:32:10 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,12 +2,22 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         return null;
     }
 
-    private String
+    private Map<String, Integer> getItemToCountMap(String skus) {
+        Map<String, Integer> itemToCountMap = new HashMap<>();
+        for(char c : skus.toCharArray()) {
+            
+        }
+    }
 
 }
+
 
