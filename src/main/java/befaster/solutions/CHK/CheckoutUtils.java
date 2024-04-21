3af8ae94693b6ee3301dbae552c500250e8b9fba
sbx_user1051 <sbx_user1051@169.254.33.101>
2024-04-21 03:23:10 +0000

    Sun Apr 21 03:23:10 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutUtils.java
+++ src/main/java/befaster/solutions/CHK/CheckoutUtils.java
@@ -1,6 +1,15 @@
 package befaster.solutions.CHK;
 
+import java.util.Map;
+
 public class CheckoutUtils {
 
-    
+    public static final Map<ItemType, ItemPrice> ItemToPriceMap =
+            Map.of(
+                    ItemType.A, new ItemPrice(50d, 130d, 3d),
+                    ItemType.B, new ItemPrice(30d, 45d, 2d),
+                    ItemType.C, new ItemPrice(20d),
+                    ItemType.D, new ItemPrice(15d)
+            );
 }
+
--- src/main/java/befaster/solutions/CHK/ItemPrice.java
+++ src/main/java/befaster/solutions/CHK/ItemPrice.java
@@ -1,2 +1,34 @@
-package befaster.solutions.CHK;public class ItemPrice {
+package befaster.solutions.CHK;
+
+import java.util.Optional;
+
+public class ItemPrice {
+    private final Double unitPrice;
+    private final Double specialOfferPrice;
+    private final Double specialOfferQuantity;
+
+    public ItemPrice(Double unitPrice, Double specialOfferPrice, Double specialOfferQuantity) {
+        this.unitPrice = unitPrice;
+        this.specialOfferPrice = specialOfferPrice;
+        this.specialOfferQuantity = specialOfferQuantity;
+    }
+
+    public ItemPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+        this.specialOfferPrice = Double.NaN;
+        this.specialOfferQuantity = Double.NaN;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public Optional<Double> getSpecialOfferPrice() {
+        return !Double.isNaN(specialOfferPrice) ? Optional.of(specialOfferPrice) : Optional.empty();
+    }
+
+    public Optional<Double> getSpecialOfferQuantity() {
+        return !Double.isNaN(specialOfferQuantity) ? Optional.of(specialOfferQuantity) : Optional.empty();
+    }
 }
+
