e5fd497f3a1656399ec6a11fc61e6058c65a8b2f
sbx_user1051 <sbx_user1051@169.254.33.101>
2024-04-21 13:45:23 +0000

    Sun Apr 21 13:45:23 UTC 2024

--- src/main/java/befaster/solutions/CHK/Catalogue.java
+++ src/main/java/befaster/solutions/CHK/Catalogue.java
@@ -11,13 +11,28 @@ public class Catalogue implements Serializable {
     public static Map<ItemType, ItemPrice> buildCatalogue(Map<Object, Object> data) {
         Map<ItemType, ItemPrice> newData = new HashMap<>();
         for (Map.Entry<Object, Object> item : data.entrySet()) {
-            ItemType keyStr = ItemType.forName((String) item.getKey());
+            ItemType itemType = ItemType.forName((String) item.getKey());
             Map<String, Object> value = (Map<String, Object>) item.getValue();
             Map<String, Object> specialOffers = ((Map<String, Object>) value.get("specialOffers"));
             if (specialOffers != null) {
                 List<Object> offers = (List<Object>) specialOffers.get("offers");
                 for (Object offer : offers) {
                     Map<String, Object> offerMap = ((Map<String, Object>) offer);
+                    Integer quantity = (Integer) offerMap.get("quantity");
+                    Integer unitPrice = (Integer) offerMap.get("unitPrice");
+                    Integer freebieUnit = (Integer) offerMap.get("freebieUnit");
+                    Integer frequency = (Integer) offerMap.get("frequency");
+                    ItemType freebieItemType = ItemType.forName((String) offerMap.get("itemType"));
+                    OfferType offerType = OfferType.forName((String) offerMap.get("offerType"));
+
+                    newData.put(
+                            itemType, new ItemPrice(50,
+                                    new SpecialOffers.SpecialOffersBuilder()
+                                            .withOffer(quantity, freebieItemType, unitPrice, offerType, freebieUnit, frequency)
+                                            .build())
+
+                    );
+
                     System.out.println(value);
                 }
                 System.out.println(value);
@@ -30,6 +45,7 @@ public class Catalogue implements Serializable {
         return newData;
     }
 }
+
 
 
 
